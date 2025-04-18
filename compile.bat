7952e4c4211342384b06c50688f4429aa1d17df4
ShrineFox <ShrineFox@users.noreply.github.com>
2025-04-17 20:36:21 -0400

    Clean up repository structure, attempt to fix hang
    Still doesn't stop the game from softlocking on Isabelle's announcements but at least now it's easier to test and hopefully get to the bottom of this issue

--- compile.bat
+++ compile.bat
@@ -1,4 +1,4 @@
 set COMPILER="../acnh-eventflow-compiler/main.py"
 set PYTHONPATH="C:\Python39\python.exe"
 
-for /r %%a in (*.evfl) do %PYTHONPATH% %COMPILER% --functions functions.csv -d EventFlow/ %%a
+for /r "EventFlow_Edited" %%a in (*.evfl) do %PYTHONPATH% %COMPILER% --functions functions.csv -d romfs/EventFlow/ %%a
